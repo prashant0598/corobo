e856d44ce83e80ceecb33c2a73fef548e7da50d0
Prashant Nigam <nigamprashant.0598@gmail.com>
2017-10-18 01:31:43 +0530

    Add Squirrel Images

--- plugins/ship_it.py
+++ plugins/ship_it.py
@@ -15,6 +15,10 @@ class Ship_it(BotPlugin):
         'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
         'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
         'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+        'https://memegenerator.net/img/instances/500x/16945547/jenkins-working-ship-it.jpg',
+        'https://memegenerator.net/img/instances/500x/54432217/executive-code-ship-it.jpg',
+        'https://lasfloressquirrels.files.wordpress.com/2010/05/squirrel-pirate.jpg',
+        'https://i.pinimg.com/564x/25/fc/a8/25fca85b96a21a308a851090c03110ff.jpg',
     ]
     # stop ignoring LineLengthBear PyCodeStyleBear
 
